9881334ab8ef119e234cc138e8b912b7269fc240
andres <prof.andresjimenez@gmail.com>
2019-12-01 23:17:04 +0000

    test

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://andres-ci-example.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
